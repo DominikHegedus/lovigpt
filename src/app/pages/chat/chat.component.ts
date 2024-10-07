52b109f097efd4230fbcd5bf219461c8522e35cc
Dominik Hegedus <hege0123@gmail.com>
2024-10-07 18:58:35 +0200

    add comment

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -19,6 +19,7 @@ import { InputComponent } from '../../ui/input/input.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChatComponent {
+  // focus on newest
   @ViewChild('focusArea') focusArea?: ElementRef<HTMLElement>;
 
   chatService = inject(ChatService);
